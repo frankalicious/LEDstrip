647c2c7a8a570e25c4f26165833c4a79c5d1f39d
frankalicious <af.frankalicious@googlemail.com>
2014-03-14 23:37:37 +0100

    add tests for some components
    add endless loop for testing
    - serial
    - pwm
    - radio nrf24l01+

--- firmware/ledstrip.c
+++ firmware/ledstrip.c
@@ -19,6 +19,9 @@
 #include "mstdio.h"
 #include "i2cmaster.h"
 #include "pca9685.h"
+#include "nRF24L01.h" //for FIFO_STATUS
+#include "mirf.h"
+#include "spi.h"
 
 /*  
     OC2B: PWM ~OE
@@ -34,6 +37,45 @@
     PD4: ~LED
 */
 
+void led(char on);
+void setledpwm(unsigned int channel, unsigned char value);
+void register_status(void);
+void endless_serial_test (void);
+void endless_pwm_test (void);
+void endless_radio_test (void);
+
+char* cmd_name[]={
+  "CONFIG",
+  "EN_AA",
+  "EN_RXADDR",
+  "SETUP_AW",
+  "SETUP_RETR",
+  "RF_CH",
+  "RF_SETUP",
+  "STATUS",
+  "OBSERVE_TX",
+  "CD",
+  "RX_ADDR_P0",
+  "RX_ADDR_P1",
+  "RX_ADDR_P2",
+  "RX_ADDR_P3",
+  "RX_ADDR_P4",
+  "RX_ADDR_P5",
+  "TX_ADDR",
+  "RX_PW_P0",
+  "RX_PW_P1",
+  "RX_PW_P2",
+  "RX_PW_P3",
+  "RX_PW_P4",
+  "RX_PW_P5",
+  "FIFO_STATUS",
+  "N/A",
+  "N/A",
+  "N/A",
+  "N/A",
+  "DYNPD",
+  "FEATURE"
+};
 
 // We don't really care about unhandled interrupts.
 EMPTY_INTERRUPT(__vector_default)
@@ -170,31 +212,232 @@ int main(void) {
   pca9685_led_pwm(0x80, 0, 0);
   pca9685_led_pwm(0x80, 1, 0);
   pca9685_led_pwm(0x80, 2, 0);
-  
+
+#if 0
+  endless_serial_test();
+#endif
+
+#if 1
+  endless_pwm_test();
+#endif
+
+#if 0
+  endless_radio_test();
+#endif
+}
+
+
+void register_status(void)
+{
+  uint8_t reg;
+  for (reg = 0; reg <=0x1d ; reg++)
+  {
+    mprintf(PSTR("\"%s\" "),cmd_name[reg]);;
+    if (reg == 0xa || reg == 0xb || reg == 0x10)
+    {
+      uint8_t value[5];
+      mirf_read_register(reg, &value[0], 5);
+      mprintf(PSTR("0x%02x 0x%02x 0x%02x 0x%02x 0x%02x\n"),
+              value[0], value[1], 
+              value[2], value[3], 
+              value[4]);
+
+      /* mprintf(PSTR("register:0x%02x value:0x%02x 0x%02x 0x%02x 0x%02x 0x%02x \n"), */
+      /*         reg,  */
+      /*         value[0], value[1],  */
+      /*         value[2], value[3],  */
+      /*         value[4]); */
+    }
+    else
+    {
+      uint8_t value;
+      mirf_read_register(reg, &value, 1);
+      mprintf(PSTR("0x%02x\n"),
+              value);
+      /* mprintf(PSTR("register:0x%02x value:0x%02x\n"), */
+      /* reg, value); */
+    }
+  }
+}
+
+void endless_serial_test (void)
+{
+  while (1)
+  {
+    _delay_ms(1000);
+    mprintf(PSTR("serial\n"));
+    wdt_reset();
+  }
+}
+
+void endless_pwm_test (void)
+{
   unsigned char frame = 0;
-  //unsigned char inten = 0;
+//unsigned char inten = 0;
+  /* uint8_t led_nr =0; */
+#if 1
   while (1) {
-    //mprintf(PSTR("F\n"));
+//mprintf(PSTR("F\n"));
     led(frame++ & 15);
     _delay_ms(10);
     wdt_reset();
-    /*
-    pca9685_led_pwm(0x80, 0, frame);
-    pca9685_led_pwm(0x80, 1, 128-frame);
-    pca9685_led_pwm(0x80, 2, 255-frame);
-    */
+/*
+  pca9685_led_pwm(0x80, 0, frame);
+  pca9685_led_pwm(0x80, 1, 128-frame);
+  pca9685_led_pwm(0x80, 2, 255-frame);
+*/
+
     setledpwm(0, 255-frame);
     setledpwm(1, frame);
     setledpwm(2, frame >> 4);
 
-    setledpwm(ON_BOARD|0, frame);
-    setledpwm(ON_BOARD|1, 255-frame);
-    setledpwm(ON_BOARD|2, frame>>4);
     if (frame & 15) {
       setledpwm(ON_BOARD|3, 0);
     } else {
       setledpwm(ON_BOARD|3, 255);
     }
-    //setledpwm(ON_BOARD|4, frame);
+
+//setledpwm(ON_BOARD|4, frame);
+  }
+#endif
+
+#if 0
+  while (1){
+    static uint8_t up=1;
+    led(frame & 15);
+    wdt_reset();
+    _delay_ms(10);
+    if (frame++ == 0)
+    {
+      if (up == 1)
+        up = 0;
+      else {
+        setledpwm(0, 0);
+        setledpwm(1, 0);
+        setledpwm(2, 0);
+        led_nr++;
+        up = 1;
+      }
+    }
+    if (led_nr == 3)
+      led_nr = 0;
+    if (up == 1 )
+      setledpwm(led_nr, frame);
+    else
+      setledpwm(led_nr, 255-frame);
+  }
+#endif
+}
+
+void endless_radio_test (void)
+{
+  spi_init();
+  mirf_init();
+// Initialize AVR for use with mirf
+  mirf_config();
+  mprintf(PSTR("1\n"));
+// Wait for mirf to come up
+  _delay_ms(50);
+  wdt_reset();
+  mprintf(PSTR("2\n"));
+// Activate interrupts
+  sei();
+  wdt_reset();
+// Configure mirf
+  mirf_config();
+  mprintf(PSTR("3\n"));
+  mprintf(PSTR("finished\n"));
+
+/* nrf24 testing */
+  register_status();
+#ifdef RECEIVE_MODE
+  mprintf(PSTR("Receive mode\n"));
+#endif
+#ifdef TRANSMIT_MODE
+  mprintf(PSTR("Transmit mode\n"));
+#endif
+#ifdef TRANSMIT_MODE
+  uint8_t buffer_tx[mirf_PAYLOAD] = {0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xA,0xB,0xC,0xD,0xE,0xF,};
+#endif
+  while (1){
+#ifdef RECEIVE_MODE
+    uint8_t buffer_rx[mirf_PAYLOAD];
+#endif
+
+#ifdef RECEIVE_MODE
+    uint8_t data_available = mirf_data_ready();
+#endif
+    uint8_t status = mirf_status();
+    static uint8_t last_status = 0xFF;
+    uint8_t fifo_status;
+    static uint8_t last_fifo_status = 0xFF;
+    uint8_t irq_status = PINC & (1<<PC0);
+    static uint8_t last_irq_status = 0xFF;
+    /* char data[] = "N123;B555;M666"; */
+    wdt_reset();
+    /* mirf_send((uint8_t*)&data[0], sizeof(data)/sizeof(data[0])); */
+    /* mirf_send((uint8_t*)&data[0], 15); */
+    mirf_read_register(FIFO_STATUS, &fifo_status, 1);
+#ifdef RECEIVE_MODE
+    if (data_available)
+    {
+      uint8_t i;
+      mirf_get_data(&buffer_rx[0]);
+      PORTD ^= _BV(PD4);   //toggle led
+      /* mprintf(PSTR("buffer: 0x%x 0x%x\n"),buffer[0], buffer[1]); */
+      mprintf(PSTR("buffer:"));
+      for (i=0; i<mirf_PAYLOAD; i++)
+      {
+        /* mprintf(PSTR(" %d:0x%x"),i, buffer_rx[i]); */
+        mprintf(PSTR(" 0x%x"),buffer_rx[i]);
+      }
+      mprintf(PSTR("\n"));
+    }
+    /* else */
+    /* { */
+    /*   mirf_flush_rx_tx(); */
+    /* } */
+#endif
+
+#ifdef TRANSMIT_MODE
+    if (mirf_max_rt_reached())
+    {
+      mprintf(PSTR("max_rt_reached\n"));
+      /* mirf_config_register(STATUS, MAX_RT); */
+      mirf_config_register(STATUS,
+                           _BV(RX_DR) | _BV(TX_DS) | _BV(MAX_RT) );
+      mirf_flush_rx_tx();
+    }
+    else
+    {
+      buffer_tx[0]++;
+    }
+    mirf_send(&buffer_tx[0],mirf_PAYLOAD);
+#endif
+
+    wdt_reset();
+    if (status != last_status)
+    {
+      mprintf(PSTR("Status:0x%x\n"),status);
+      last_status = status;
+    }
+    if (fifo_status != last_fifo_status)
+    {
+      mprintf(PSTR("FIFO Status:0x%x\n"),fifo_status);
+      last_fifo_status = fifo_status;
+    }
+    if (irq_status != last_irq_status)
+    {
+      mprintf(PSTR("IRQ:0x%x\n"),irq_status);
+      last_irq_status = irq_status;
+    }
+/* delay for receive should be lesser than for transmit */
+#ifdef TRANSMIT_MODE
+    /* _delay_ms(10); */
+    _delay_ms(1000);
+#endif
+/* #ifdef RECEIVE_MODE */
+    /* _delay_ms(100); */
+/* #endif */
   }
 }
